9f3d19c0db61a9d02e68cc84ff213193878e5fa8
unknown <dele17822@uvg.edu.gt>
2019-11-20 19:57:09 -0600

    Proyecto Final Discreta

---
+++ ProyectoFinal.py
@@ -0,0 +1,75 @@
+'''
+
+			Proyecto Final
+			Matematica Discreta
+			Seccion 20
+
+Creado por:
+
+@author: Fatima Albeño					18060
+@author: Juan Fernando De Leon Quezada	17822
+@author: Jose Amado Garcia				181469
+
+Programa capaz de listar todas las particiones de longitud K de cualquier numero entero N.
+
+'''
+
+def partitionsOf(n):
+    a = [0 for i in range(n + 1)]
+    k = 1
+    y = n - 1
+    while k != 0:
+        x = a[k - 1] + 1
+        k -= 1
+        while 2 * x <= y:
+            a[k] = x
+            y -= x
+            k += 1
+        l = k + 1
+        while x <= y:
+            a[k] = x
+            a[l] = y
+            yield a[:k + 2]
+            x += 1
+            y -= 1
+        a[k] = x + y
+        y = x + y - 1
+        yield a[:k + 1]
+
+def mainMenu():
+	return '''
+	\n\tMenu Principal
+	\n1. Calcular particiones
+	\n2. Salir
+	'''
+
+def calculatePartitions():
+	
+	n = input("\n\nIngrese un numero N: ")
+	particiones = partitionsOf(int(n))
+	#print("\nExisten", len(list(particiones)), "particiones del numero", n)
+
+	k = input("\nIngrese longitud de particiones a desplegar: ")
+	for i in particiones:
+		if int(k) == 0:
+			print(i)
+		elif len(i) == int(k):
+			print(i)
+
+
+wantsToContinue = True
+
+while wantsToContinue:
+
+	print(mainMenu())
+	
+	op = input("\nIngrese una opcion: ")
+
+	if op == '1':
+		calculatePartitions()
+	elif op == '2':
+		wantsToContinue = False
+	else:
+		print("\nERROR: La opcion ingresada no es valida.")
+
+
